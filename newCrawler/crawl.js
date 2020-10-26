38b0547c80a64247d01a537cfe56350ca2713efb
Raiyan Rahman <ray.rahmn@hotmail.com>
2020-10-25 21:01:36 -0400

    Added structure to output JSON.

--- newCrawler/crawl.js
+++ newCrawler/crawl.js
@@ -5,7 +5,7 @@
    dynamically rendered webpages and returns the JSON file containing lists
    of tuples of links and titles for each url.
    Use: "node crawl.js -l <url1> ..."
-   Output: link_title_list.json
+   Output: link_title_list.json, failed_link_list.json
 */
 const Apify = require('apify');
 const path = require('path');
@@ -21,6 +21,7 @@ console.log = function(d) {
   log_file.write(util.format(d) + '\n');
   log_stdout.write(util.format(d) + '\n');
 };
+
 function Filter(url, domain_url, valid_links) {
     // Checks that the url has the domain name, it is not a repetition or it is not the same as the original url
     result = !valid_links.includes(url) && (url != domain_url)
@@ -36,16 +37,8 @@ function Filter(url, domain_url, valid_links) {
         }
     }*/
     return result
-}
 
-function getParsedArticle(url, html) {
-    var doc = new JSDOM(html, {
-        url: url
-      });
-    let reader = new Readability(doc.window.document);
-    let article = reader.parse();
-    return article
-}
+};
 
 function getParsedArticle(url, html) {
     var doc = new JSDOM(html, {
@@ -70,18 +63,18 @@ Apify.main(async () => {
             is_url = true;
         }
       });
-    console.log(url_list);  // Ouput the links provided.
 
     // Create the JSON object to store the tuples of links and titles for each url.
     var output_dict = {};
     var incorrect_dict = {};
+
     const requestQueue = await Apify.openRequestQueue();
     // Add the links to the queue of websites to crawl.
     for (var i = 0; i < url_list.length; i++) {
         await requestQueue.addRequest({ url: url_list[i] });
     }
     // Crawl the deeper URLs recursively.
-    // const pseudoUrls = [new Apify.PseudoUrl('https://www.idf.il/en/[.*]')];
+    const pseudoUrls = [new Apify.PseudoUrl('https://www.aljazeera.com/[.*]')];
 
     // Initialize the crawler.
     const crawler = new Apify.PuppeteerCrawler({
@@ -92,7 +85,7 @@ Apify.main(async () => {
             useChrome: false,
         },
         handlePageFunction: async ({ request, page }) => {
-            const title = await page.title();   // Get the title of the page.
+
             let general_regex = /(http(s)?:\/\/(www\.)?)([^.]+)((\.[a-zA-Z]+)+)/;
             let match = request.url.match(general_regex);
             let link_start = "";
@@ -100,26 +93,22 @@ Apify.main(async () => {
             // anything before the domain name.
             //link_start = match[1];
             domainName = link_start+match[4]+ ".";
-            console.log(`Title of "${request.url}" is "${title}"`);
+
+
+            const title = await page.title();   // Get the title of the page.
             // Get the HTML of the page and write it to a file.
             let bodyHTML = await page.evaluate(() => document.body.innerHTML);   // Get the HTML content of the page.
 
             // Use readability.js to read information about the article.
             parsedArticle = getParsedArticle(request.url, bodyHTML);
-            console.log("");
-            console.log("URL: " + request.url);
-            console.log("Article Title: " + parsedArticle.title);
-            console.log("Article Byline: " + parsedArticle.byline);
-            console.log("Article Text Content: " + parsedArticle.textContent);
-            console.log("Article Length: " + parsedArticle.length);
-            console.log("Article Excerpt: " + parsedArticle.excerpt);
-            console.log("Article HTML Content: " + parsedArticle.content);
-            console.log("");
 
             const hrefs = await page.$$eval('a', as => as.map(a => a.href));    // Get all the hrefs with the links.
             const titles = await page.$$eval('a', as => as.map(a => a.title));  // Get the titles of all the links.
             const texts = await page.$$eval('a', as => as.map(a => a.text));    // Get the text content of all the a tags.
             
+            // Create the list of tuples for this url.
+            var tuple_list = [];
+            // Set the title of the link to be the text content if the title is not present.
             // Create the list of tuples for this url.
             var valid_links = [];
             var tuple_list = [];
@@ -154,8 +143,18 @@ Apify.main(async () => {
 
                 }
             }
+            var elem = {
+                title: parsedArticle.title,
+                author_metadata: parsedArticle.byline,
+                date: '',
+                html_content: parsedArticle.content,
+                article_text: parsedArticle.textContent,
+                article_len: parsedArticle.length,
+                url: request.url,
+                found_urls: tuple_list
+            }
             // Add this list to the dict.
-            output_dict[request.url] = tuple_list;
+            output_dict[request.url] = elem;
 
             // Enqueue the deeper URLs to crawl.
             await Apify.utils.enqueueLinks({ page, selector: 'a', pseudoUrls, requestQueue });
@@ -171,7 +170,6 @@ Apify.main(async () => {
     // Note: If the apify_storage file is not removed, it doesn't crawl
     // during subsequent runs.
     // Implementation of rmdir.
-    console.log(JSON.stringify(output_dict));
     const rmDir = function (dirPath, removeSelf) {
     if (removeSelf === undefined)
         removeSelf = true;
