f48aaafb208b0855d7d39cede4e9af729f5655e8
Jacqueline <jacquelinechan2@hotmail.com>
2020-11-06 23:56:44 -0500

    eslint for getDate.js

--- newCrawler/getDates.js
+++ newCrawler/getDates.js
@@ -30,15 +30,15 @@ async function promiseLoop(json) {
     let newJson = await promiseLoopAsync(json);
     console.log(newJson);
 
-    console.log("we are done! :)");
+    console.log('we are done! :)');
 
-    fs.writeFile("metadata_modified_list.json", JSON.stringify(newJson), function(err) {
+    fs.writeFile('metadata_modified_list.json', JSON.stringify(newJson), function(err) {
         // console.log(newJson)
         if (err) {
-            console.log("The date json has been processed with an error")
-            console.log(err)
+            console.log('The date json has been processed with an error');
+            console.log(err);
         } else {
-            console.log("complete!")
+            console.log('complete!');
         }
     });
 }
@@ -47,28 +47,25 @@ async function promiseLoop(json) {
 async function promiseLoopAsync (json) {
 
     let newJson = {};
-    // let newCount = 0;
-    let count = 0;
     let key;
 
     for (key in json) {
         let articlesList = json[key];
         let promises = [];
-        count++; 
         let i;
         for (i = 0; i < articlesList.length; i++) {
-            let article = articlesList[i]
-            promises.push(promiseDate(article))
+            let article = articlesList[i];
+            promises.push(promiseDate(article));
         }
         await Promise.allSettled(promises).then( (promiseResults) => {
-            let articleListMetadata = []
+            let articleListMetadata = [];
             promiseResults.forEach(
                 (result) => {
-                    if (result.status === "fulfilled") {
-                        let link = result.value[0]
-                        let date = result.value[result.value.length - 1]
+                    if (result.status === 'fulfilled') {
+                        let link = result.value[0];
+                        let date = result.value[result.value.length - 1];
                         // console.log("result : ", result)
-                        console.log('Valid link ' + link  + "with date " + date);
+                        console.log('Valid link ' + link  + 'with date ' + date);
                         articleListMetadata.push(result.value);
                     } else {
                         // console.log("result : ", result)
@@ -76,17 +73,17 @@ async function promiseLoopAsync (json) {
                         articleListMetadata.push(result.reason);
                     }
                 }
-            )
+            );
 
-            newJson[key] = articleListMetadata
+            newJson[key] = articleListMetadata;
             
         }).catch ((e) =>
             console.log(e)
-        )
+        );
 
     }
 
-    return newJson
+    return newJson;
 
 }
 
@@ -97,27 +94,27 @@ function promiseDate (article) {
     let promise = new Promise ((resolve, reject) => {
         let link = article[0];
         got(link)
-        .on('request', request => setTimeout(() => request.destroy(), 20000))
-        .then(({ body: html, url }) => {
-            metascraper({ html, url }).then((metadata) => {
-                console.log("metadata : ", metadata);
-                console.log("Pass: "+link+ " "+metadata["date"]);
-                article.push(metadata["date"])
-                resolve(article); 
-            }).catch ((e) => {
-                console.log("error : ", e.hostname);
-                console.log("Fail: "+link);
-                article.push("")
+            .on('request', request => setTimeout(() => request.destroy(), 20000))
+            .then(({ body: html, url }) => {
+                metascraper({ html, url }).then((metadata) => {
+                    console.log('metadata : ', metadata);
+                    console.log('Pass: '+link+ ' '+metadata['date']);
+                    article.push(metadata['date']);
+                    resolve(article); 
+                }).catch ((e) => {
+                    console.log('error : ', e.hostname);
+                    console.log('Fail: '+link);
+                    article.push('');
+                    reject(article);
+                }); 
+            }).catch((e) => {
+                console.log('error : ', e.hostname);
+                console.log('Fail: at get request: ' + link);
+                article.push('');
                 reject(article);
-            }); 
-        }).catch((e) => {
-            console.log("error : ", e.hostname);
-            console.log("Fail: at get request: " + link)
-            article.push("")
-            reject(article);
-        });
+            });
     });
-    return promise
+    return promise;
 }
 
 if (require.main === module) {
